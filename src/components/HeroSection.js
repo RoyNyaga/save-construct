3dd7cb2fcdb4e689e3541b84777c3eb7b7ba1fba
RoyNyaga <nyagaandreroy@gmail.com>
2020-11-07 22:37:57 +0100

    commit of the day

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,8 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <h1>SAVE CONSTRUCT</h1>
+      <p>Receive weekly updates about the cost and state of your construction project.</p>
       <div className='hero-btns'>
         <Button
           className='btns'
